e82cb10dc164930b55b21a6b283e9ec86da994d5
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2019-06-14 20:05:26 -0700

    another redirect change

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -44,7 +44,7 @@ class LoginController @Inject() (userMatcher: UserMatcher, cc: MessagesControlle
     val success = { data: LoginData =>
 
       if (userMatcher.validUser(data.username, data.password)) {
-        Redirect(data.redirectUrl.getOrElse("/"), SEE_OTHER).withSession(request.session + ("authenticated" -> "ok") + ("user" -> data.username))
+        Redirect(data.redirectUrl.getOrElse("/"), FOUND).withSession(request.session + ("authenticated" -> "ok") + ("user" -> data.username))
       } else {
         Logger.warn(s"Bad login attempt for user ${data.username} from ${request.remoteAddress}")
         Unauthorized(views.html.login(loginForm.fill(data.copy(password = "")), request.session.get("redirectUrl").getOrElse(""), routes.LoginController.login()))
