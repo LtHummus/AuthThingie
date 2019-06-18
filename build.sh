e143dc57b7f01d0b5f781664b4538c9de38477da
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2019-06-17 22:56:36 -0700

    run tests as a part of the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt universal:packageZipTarball && docker-compose build && docker-compose up -d && docker-compose logs -f auth
+sbt test && sbt universal:packageZipTarball && docker-compose build && docker-compose up -d && docker-compose logs -f auth
