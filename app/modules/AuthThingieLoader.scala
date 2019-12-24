ebb32a29f191dc2923405f9ecf96ae7cbf1c9c41
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2019-12-23 20:57:56 -0800

    read key from env var if available

--- app/modules/AuthThingieLoader.scala
+++ app/modules/AuthThingieLoader.scala
@@ -13,6 +13,8 @@ class AuthThingieLoader extends GuiceApplicationLoader() {
   private val MinSecretKeyLength = 16
   private val SecretKeyConfigPath = "play.http.secret.key"
 
+  private def secretKeyIsBad(key: String): Boolean = key.length < MinSecretKeyLength || key.equalsIgnoreCase("SAMPLE_SECRET_KEY")
+
   override def builder(context: ApplicationLoader.Context): GuiceApplicationBuilder = {
     val additionalConfig = sys.env.get("AUTHTHINGIE_CONFIG_FILE_PATH") match {
       case Some(filePath) =>
@@ -22,11 +24,11 @@ class AuthThingieLoader extends GuiceApplicationLoader() {
     }
 
     val combinedConfiguration = Configuration(additionalConfig).withFallback(context.initialConfiguration)
-    val configPatch = if (combinedConfiguration.get[String](SecretKeyConfigPath).length < MinSecretKeyLength) {
-      Logger.warn(s"Hey! Listen! Your secret key is crap! It should be at least 16 characters long. The fundamental security " +
-        s"of the app relies on it. I've randomly generated one for you for now, but this means that sessions will be invalidated " +
+    val configPatch = if (secretKeyIsBad(combinedConfiguration.get[String](SecretKeyConfigPath))) {
+      Logger.warn(s"Hey! Listen! Your secret key is crap! It should be at least 16 characters long and not the default that I ship with. " +
+        s"The fundamental security of the app relies on it. I've randomly generated one for you for now, but this means that sessions will be invalidated " +
         s"when you restart the app. You should set (a strong) one manually by setting the config key `play.http.secret.key` in your " +
-        s"config file.")
+        s"config file or by setting the `AUTHTHINGIE_SECRET_KEY` environment variable.")
 
       val generatedSecretKey = RandomStringUtils.randomAlphanumeric(MinSecretKeyLength, MinSecretKeyLength * 2)
       Configuration(SecretKeyConfigPath -> generatedSecretKey)
