2b5d0e15851f8438fe0594f9a7997348779ff073
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2019-12-23 20:26:15 -0800

    actually load config from path specified in env var

--- app/modules/AuthThingieLoader.scala
+++ app/modules/AuthThingieLoader.scala
@@ -1,16 +1,28 @@
 package modules
 
+import java.io.File
+
+import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 import play.api.{ApplicationLoader, Configuration}
 import play.api.inject.guice.{GuiceApplicationBuilder, GuiceApplicationLoader}
 
 class AuthThingieLoader extends GuiceApplicationLoader() {
+  private val Logger = LoggerFactory.getLogger(getClass)
 
   override def builder(context: ApplicationLoader.Context): GuiceApplicationBuilder = {
-    val additionalConfig = Configuration("foo" -> "bar")
+    val additionalConfig = sys.env.get("AUTHTHINGIE_CONFIG_FILE_PATH") match {
+      case Some(filePath) =>
+        Logger.debug(s"Loading fallback config from $filePath")
+        ConfigFactory.parseFile(new File(filePath))
+      case None => throw new IllegalStateException(s"AUTHTHINGIE_CONFIG_FILE_PATH is not set")
+    }
+
+    val combinedConfiguration = context.initialConfiguration.withFallback(Configuration(additionalConfig))
 
     initialBuilder
       .in(context.environment)
-      .loadConfig(context.initialConfiguration.withFallback(additionalConfig))
+      .loadConfig(combinedConfiguration)
       .overrides(overrides(context): _*)
 
   }
