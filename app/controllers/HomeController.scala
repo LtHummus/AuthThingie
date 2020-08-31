2d138022574253f2bbfb08646e49f33c54a605a7
Benjamin Schwartz <LtHummus@users.noreply.github.com>
2020-08-30 17:17:47 -0700

    minor code cleanup

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -12,17 +12,6 @@ import util.SessionImplicits._
 class HomeController @Inject()(userMatcher: UserMatcher,
                                cc: MessagesControllerComponents)(implicit config: AuthThingieConfig) extends MessagesAbstractController(cc) {
 
-  private val Logger = play.api.Logger(this.getClass)
-
-
-
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
     val loggedInUser = for {
       sessionUser      <- request.session.getUserAuthedWithin(config.sessionTimeout)
